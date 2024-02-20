9dbd06c3a6250da62da90427c8b519c8122230e7
Tom Lausmann <tomlausmann@gmail.com>
2024-02-20 20:09:17 +0100

    feat: add logging, ci and config

--- src/main/kotlin/de/lausi/gsvdiscordtools/rankedwatcher/adapter/riot/RiotPlayerResolver.kt
+++ src/main/kotlin/de/lausi/gsvdiscordtools/rankedwatcher/adapter/riot/RiotPlayerResolver.kt
@@ -1,6 +1,7 @@
 package de.lausi.gsvdiscordtools.rankedwatcher.adapter.riot
 
 import de.lausi.gsvdiscordtools.rankedwatcher.domain.model.player.*
+import org.slf4j.LoggerFactory
 import org.springframework.web.client.RestTemplate
 
 private data class RiotPlayerResource(
@@ -11,13 +12,19 @@ private data class RiotPlayerResource(
 
 internal class RiotPlayerResolver(val restTemplate: RestTemplate) : PlayerResolver {
 
+  private val log = LoggerFactory.getLogger(RiotPlayerResolver::class.java)
+
   override fun getPlayer(summonerName: SummonerName, tagLine: TagLine): Player? {
     val url = "/riot/account/v1/accounts/by-riot-id/${summonerName.value}/${tagLine.value}"
+    log.info("Fetching player from riot: $url")
     val response = restTemplate.getForEntity(url, RiotPlayerResource::class.java)
 
-    return response.body?.let {
+    val player = response.body?.let {
       val summonerId = SummonerId(it.puuid)
       Player(summonerId, summonerName, tagLine)
     }
+    log.info("Loaded player from riot: $player")
+
+    return player
   }
 }
