96b8f67325c856f5302f5fecff100fcdc24a02c1
Tom Lausmann <tomlausmann@gmail.com>
2024-02-20 20:23:23 +0100

    feat: remove too verbose logging

--- src/main/kotlin/de/lausi/gsvdiscordtools/rankedwatcher/adapter/riot/RiotPlayerResolver.kt
+++ src/main/kotlin/de/lausi/gsvdiscordtools/rankedwatcher/adapter/riot/RiotPlayerResolver.kt
@@ -16,14 +16,12 @@ internal class RiotPlayerResolver(val restTemplate: RestTemplate) : PlayerResolv
 
   override fun getPlayer(summonerName: SummonerName, tagLine: TagLine): Player? {
     val url = "/riot/account/v1/accounts/by-riot-id/${summonerName.value}/${tagLine.value}"
-    log.info("Fetching player from riot: $url")
     val response = restTemplate.getForEntity(url, RiotPlayerResource::class.java)
 
     val player = response.body?.let {
       val summonerId = SummonerId(it.puuid)
       Player(summonerId, summonerName, tagLine)
     }
-    log.info("Loaded player from riot: $player")
 
     return player
   }
