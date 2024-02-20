cdcde294952765497fb7290c4a5a046a36483b4e
Tom Lausmann <tom.lausmann@unitb.de>
2024-02-20 12:54:46 +0100

    Initial Commit

---
+++ src/main/kotlin/de/lausi/gsvdiscordtools/GsvDiscordToolsApplication.kt
@@ -0,0 +1,11 @@
+package de.lausi.gsvdiscordtools
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GsvDiscordToolsApplication
+
+fun main(args: Array<String>) {
+  runApplication<GsvDiscordToolsApplication>(*args)
+}
---
+++ src/test/kotlin/de/lausi/gsvdiscordtools/GsvDiscordToolsApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.lausi.gsvdiscordtools
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GsvDiscordToolsApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
